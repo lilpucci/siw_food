912048b6afd6546a76d368991272e0c9b86c9c10
andrea <andrealippo01@gmail.com>
2024-05-07 12:07:25 +0200

    inizializzazione progetto 2

---
+++ siwfood/src/test/java/it/uniroma3/siwfood/siwfood/SiwfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwfood.siwfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
